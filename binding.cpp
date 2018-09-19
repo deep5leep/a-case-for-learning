86ba60573047533a6fbeb037456f4889965ddb87
deep5leep <39327077+deep5leep@users.noreply.github.com>
2018-09-19 10:45:52 +0800

    Create binding.cpp

---
+++ binding.cpp
@@ -0,0 +1,34 @@
+/*
+  this is a case about static binding and dynamic binding
+  that can help u to understand the difference of its
+*/
+#include<iostream>
+using namespace std;
+
+class A {
+public:
+      virtual void f() const {
+              cout << "A::f()" << endl;
+      }
+};
+
+class B: public A {
+public:
+      virtual void f() const {
+              cout << "B::f()" << endl;
+      }      
+};
+
+int main() {
+    B b1, b2, b3;
+    A a = b1;
+    A *p1 = &b2;
+    A &p2 = b3;
+    p1->f();//dynamic binding
+    p2.f();//dynamic binding
+    a.f();//static binding
+    
+    int t;
+    cin >> t;
+    return 0;   
+}
